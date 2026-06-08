3a872bc587504c7be2627f03218c0fa2565a5088
Giorubio27 <Giorubio27@gmail.com>
2026-06-07 19:11:18 -0600

    I added the invoices and artisan information in the dashboard segment

--- nextjs-dashboard/app/dashboard/page.tsx
+++ nextjs-dashboard/app/dashboard/page.tsx
@@ -1,3 +1,32 @@
-export default function Page() {
-  return <p>Dashboard Page</p>;
+import { Card } from '@/app/ui/dashboard/cards';
+import RevenueChart from '@/app/ui/dashboard/revenue-chart';
+import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
+import { playfairDisplay } from '@/app/ui/fonts';
+import { fetchCardData, fetchLatestInvoices } from '../lib/data';
+ 
+export default async function Page() {
+  const cardData = await fetchCardData();
+  const latestInvoices = await fetchLatestInvoices();
+
+  return (
+    <main>
+      <h1 className={`${playfairDisplay.className} mb-4 text-xl md:text-2xl`}>
+        Dashboard
+      </h1>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        <Card title="Collected" value={cardData.totalPaidInvoices} type="collected" />
+        <Card title="Pending" value={cardData.totalPendingInvoices} type="pending" />
+        <Card title="Total Invoices" value={cardData.numberOfInvoices} type="invoices" />
+        <Card
+          title="Total Artisans"
+          value={cardData.numberOfArtisans}
+          type="artisans"
+        /> 
+      </div>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        {/* <RevenueChart revenue={revenue}  /> */}
+        {/* <LatestInvoices latestInvoices={latestInvoices} /> */}
+      </div>
+    </main>
+  );
 }
