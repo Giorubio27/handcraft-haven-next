18f243e1140b6fa1a42d383b9edbf09aff3e41a3
Giorubio27 <Giorubio27@gmail.com>
2026-06-16 20:22:49 -0600

    i added lint

--- nextjs-dashboard/app/dashboard/collections/page.tsx
+++ nextjs-dashboard/app/dashboard/collections/page.tsx
@@ -1,3 +1,38 @@
-export default function Page() {
-  return <p>Collections Page</p>;
+import Pagination from '@/app/ui/invoices/pagination';
+import Search from '@/app/ui/search';
+import ArtisansTable from '@/app/ui/artisans/table';
+import { CreateArtisan } from '@/app/ui/artisans/buttons';
+import { playfairDisplay } from '@/app/ui/fonts';
+import { ArtisansSkeleton } from '@/app/ui/skeletons';
+import { Suspense } from 'react';
+import { fetchInvoicesPages } from '@/app/lib/data';
+ 
+export default async function Page(props: {
+  searchParams?: Promise<{
+    query?: string;
+    page?: string;
+  }>;
+}) {
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || '';
+  const currentPage = Number(searchParams?.page) || 1;
+  const totalPages = await fetchInvoicesPages(query);
+  
+  return (
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <h1 className={`${playfairDisplay.className} text-2xl`}>Artisans</h1>
+      </div>
+      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+        <Search placeholder="Search Artisans..." />
+        <CreateArtisan />
+      </div>
+       <Suspense key={query + currentPage} fallback={<ArtisansSkeleton />}>
+        <ArtisansTable query={query} currentPage={currentPage} />
+      </Suspense>
+      <div className="mt-5 flex w-full justify-center">
+        <Pagination totalPages={totalPages} />
+      </div>
+    </div>
+  );
 }
