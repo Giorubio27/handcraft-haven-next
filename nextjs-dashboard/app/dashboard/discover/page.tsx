031f9ffe34e7a6b1ff133dc0c4bf3eac259559e5
Giorubio27 <Giorubio27@gmail.com>
2026-06-18 09:47:58 -0600

    added discover skeleton

--- nextjs-dashboard/app/dashboard/discover/page.tsx
+++ nextjs-dashboard/app/dashboard/discover/page.tsx
@@ -3,7 +3,7 @@ import Search from '@/app/ui/search';
 import ArtisansTable from '@/app/ui/artisans/table';
 import { CreateArtisan } from '@/app/ui/artisans/buttons';
 import { playfairDisplay } from '@/app/ui/fonts';
-import { ArtisansSkeleton } from '@/app/ui/skeletons';
+import { DiscoverSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
 import { fetchInvoicesPages } from '@/app/lib/data';
  
@@ -21,14 +21,14 @@ export default async function Page(props: {
   return (
     <div className="w-full">
       <div className="flex w-full items-center justify-between">
-        <h1 className={`${playfairDisplay.className} text-2xl`}>Artisans</h1>
+        <h1 className={`${playfairDisplay.className} text-2xl`}>Discover with us</h1>
       </div>
       <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
-        <Search placeholder="Search Artisans..." />
-        <CreateArtisan />
+        
+        
       </div>
-       <Suspense key={query + currentPage} fallback={<ArtisansSkeleton />}>
-        <ArtisansTable query={query} currentPage={currentPage} />
+       <Suspense key={query + currentPage} fallback={<DiscoverSkeleton />}>
+        
       </Suspense>
       <div className="mt-5 flex w-full justify-center">
         <Pagination totalPages={totalPages} />
