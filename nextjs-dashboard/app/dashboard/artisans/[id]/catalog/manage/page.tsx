0c54f6dd3232a340bcc010067fd6ad7bfcbf7a5c
Giorubio27 <Giorubio27@gmail.com>
2026-06-18 22:46:13 -0600

    what i have to turn in

--- nextjs-dashboard/app/dashboard/artisans/[id]/catalog/manage/page.tsx
+++ nextjs-dashboard/app/dashboard/artisans/[id]/catalog/manage/page.tsx
@@ -1,24 +1,24 @@
-// app/dashboard/artisans/[id]/catalog/manage/page.tsx
-import Breadcrumbs from '@/app/ui/artisans/breadcrumbs';
-import AddCatalogItemForm from '@/app/ui/artisans/add-catalog-form';
+// // app/dashboard/artisans/[id]/catalog/manage/page.tsx
+// import Breadcrumbs from '@/app/ui/artisans/breadcrumbs';
+// import AddCatalogItemForm from '@/app/ui/artisans/add-catalog-form';
 
-export default async function Page(props: { params: Promise<{ id: string }> }) {
-  const params = await props.params;
-  const id = params.id;
+// export default async function Page(props: { params: Promise<{ id: string }> }) {
+//   const params = await props.params;
+//   const id = params.id;
 
-  return (
-    <main className="max-w-2xl mx-auto p-4 md:p-6">
-      <Breadcrumbs
-        breadcrumbs={[
-          { label: 'Artisans', href: '/dashboard/artisans' },
-          { label: 'Profile', href: `/dashboard/artisans/${id}` },
-          { label: 'Manage Catalog', href: `/dashboard/artisans/${id}/catalog/manage`, active: true },
-        ]}
-      />
+//   return (
+//     <main className="max-w-2xl mx-auto p-4 md:p-6">
+//       <Breadcrumbs
+//         breadcrumbs={[
+//           { label: 'Artisans', href: '/dashboard/artisans' },
+//           { label: 'Profile', href: `/dashboard/artisans/${id}` },
+//           { label: 'Manage Catalog', href: `/dashboard/artisans/${id}/catalog/manage`, active: true },
+//         ]}
+//       />
 
-      {/* Render the clean client form component down below */}
-      <AddCatalogItemForm artisanId={id} />
+//       {/* Render the clean client form component down below */}
+//       <AddCatalogItemForm artisanId={id} />
       
-    </main>
-  );
-}
+//     </main>
+//   );
+// }
